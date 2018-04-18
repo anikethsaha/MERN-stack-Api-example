39912625e9857277bc6b5bd8f36436e2d9c58d4f
anikethsaha <anik220798@gmail.com>
2018-04-18 12:42:20 +0530

    Update header.js

--- components/header.js
+++ components/header.js
@@ -3,14 +3,29 @@ import React from 'react';
 export default class Header extends React.Component{
   constructor(){
     super();
+    this.handleAddFakeUserPanel = this.handleAddFakeUserPanel.bind(this);
+    this.state = {isPanelBtnClicked : false};
   }
+  handleAddFakeUserPanel(e){
+    console.log(e);
+  $('.right_adduser_panel').toggleClass('open');
+  }
+
   render(){
     return (
       <header className="header">
-          <div>
-              <h1>FakeUser</h1>
-          </div>
+          <div className="container">
+                  <div className="row">
+                      <div className="seven columns">
+                          <h1>FakeUser</h1>
+                      </div>
+
+                      <div className="five columns adduser_lnk_btn">
+                          <a className="btn" onClick={this.handleAddFakeUserPanel} >Add Fake User </a>
+                      </div>
+                  </div >
+            </div>
       </header>
-    )
+    );
   }
 }
