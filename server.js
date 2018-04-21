7c6a3e1ab200efa724d1264d72af7a5cb70c4f11
anikethsaha <anik220798@gmail.com>
2018-04-21 15:59:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ var app = express();
 app.use(express.static("."));
 var path = require('path');
 var bodyparser= require('body-parser');
+var jwt = require('jsonwebtoken');
+
 var server = app.listen(8080,function(d){
   console.log("server running in 8080");
 });
+
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fakeuser');
 
@@ -35,31 +38,88 @@ app.get('/',(req,res)=>{
 var User = require('./model/user');
 app.get('/api/get/all',(req,res)=>{
     User.find({},(err,data) => {
-      // res.json("inside this");
+
       if (err) {
-        // console.error(err);
+
         res.json({
           message:"error"
         });
       }
-      // console.log(data.toArray);
 
+      console.log(data);
       res.json(data);
       });
-      // data.map( d => {
-      //   console.log(d);
-      //   res.json(d);
-      // });
 
-    // // })
-    // res.json({
-    //   msg:"hello",
-    //   type : true,
-    // });
+
+
+});
+app.post('/api/get',(req,res)=>{
+  console.log(req.body.name);
+  User.find({name:new RegExp(req.body.name,'i')},(err,data) => {
+
+    if (err) {
+
+      res.json({
+        message:"error"
+      });
+    }
+
+
+    res.json(data);
+    });
+})
+
+
+app.post('/w/login',(req,res) => {
+  console.log(req.body.name);
+  const userData = {
+    name :req.body.name,
+    password : "password"
+  }
+  jwt.sign({user : userData}, 'fakeuser',(err,token) =>{
+
+    const response = {
+        success: true,
+        token: `Bearer ${token}`
+      }
+      // console.log(response.token);
+    if(err){
+      res.sendStatus(400);
+    }
+
+    //setting the auth header
+    res.setHeader('authorization', response.token);
+    res.json("success logined");
+  })
 });
 
+
+
+// Checking token present or not middleware
+function verifyTokenHeader(req,res,next){
+  // console.log(req);
+  const bearerHeader = req.headers['authorization']; // getting the bearer token
+  if(typeof bearerHeader !== 'undefined'){ // checking the type
+      const bearer = bearerHeader.split(' ');  //spiltting by space
+      const jwtToken = bearer[1]; // extracting the token
+      req.token = jwtToken; // passing the token to resuest
+      next(); // passing this request waiting for next request
+  }else {
+    res.sendStatus(403); // forbidden
+  }
+
+}
+
+//protected route
+app.post('/w/home',verifyTokenHeader,(req,res) => {
+
+    res.json(jwt.verify(req.token,'fakeuser',(err, data) => data ));
+})
+
+
+
 app.post('/api/user/insert',(req,res)=>{
-  console.log(req.body.name);
+
   var user = new User();
     user.name = req.body.name;
     user.job = req.body.job;
@@ -76,3 +136,6 @@ app.post('/api/user/insert',(req,res)=>{
     })
 
 })
+// app.get('/*', (req, res) => {
+//  res.sendFile(path.join(__dirname, 'public/views/index.ejs'));
+// });
