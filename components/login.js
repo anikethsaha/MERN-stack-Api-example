e66d6850bb37e9537d4d18c2624f70731a68771f
anikethsaha <anik220798@gmail.com>
2018-04-22 14:29:46 +0530

    Add files via upload

---
+++ components/login.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Paper from 'material-ui/Paper';
+import Header from './header';
+import BottomNavigationExampleSimple from './bottomNavigation';
+export default class LoginComponent extends React.Component{
+  constructor(props){
+    super(props);
+    console.log("inside LoginComponent");
+  }
+  render(){
+
+
+      return (
+        <div>
+          <Header />
+            <div className="container">
+              <div className="login-bar">
+                <div className="container">
+                  <div className="row">
+                      <div className="seven columns login-details">
+                        <h1>Login Here </h1>
+                        <p> Login to add fake user and contribute to the project</p>
+                      </div>
+                      <div className="five columns login-div">
+                          <form  >
+                                <input type="text" className="first-field" name="name"placeholder="Write the name" />
+                                <input type="password" className="first-field" name="location" placeholder="Write the password"/>
+                                <button type="submit" name="button" className="btn">Submit</button>
+                          </form>
+                      </div>
+                   </div>
+                </div>
+              </div>
+            </div>
+            <BottomNavigationExampleSimple />
+        </div>
+      );
+  }
+}
