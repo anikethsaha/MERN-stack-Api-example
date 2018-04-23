e048a6710f6471d33352990cab013df8ae3fd039
anikethsaha <anik220798@gmail.com>
2018-04-23 22:01:23 +0530

    Update userform.js

--- components/userform.js
+++ components/userform.js
@@ -1,19 +1,84 @@
 import React from 'react';
-
-export default class UserForm extends React.Component{
+import { connect} from  'react-redux';
+ class UserForm extends React.Component{
   constructor(){
     super();
+    this.submitFakeUserInsert = this.submitFakeUserInsert.bind(this);
+    this.name = React.createRef();
+    this.location = React.createRef();
+    this.age = React.createRef();
+    this.gender = React.createRef();
+    this.job = React.createRef();
+    this.state = {
+      error : []
+    }
+
   }
+  componentWillMount() {
+    // console.log(this.state.eg);
+  }
+  submitFakeUserInsert(e){
+    e.preventDefault();
+    const fakeUserData = {
+      name : this.name.current.value,
+      location : this.location.current.value ,
+      age : this.age.current.value,
+      gender : this.gender.current.value,
+      job : this.job.current.value
+
+    }
+    if(localStorage.getItem('token') !== null && localStorage.hasOwnProperty('token')){
+          fetch("/api/user/insert",{
+            body:JSON.stringify(fakeUserData),
+            method : "POST",
+            headers :{
+              'content-type' : 'application/json',
+              'authorization' : localStorage.getItem('token')
+            }
+
+          }).then(res => res.json()).then((res) => {
+              console.log(res);
+          })
+    }else{
+      this.setState({
+        error : "Please Login First"
+      })
+
+    }
+
+  }
+
   render(){
+    const style = {
+      color : "red"
+    }
     return (
-      <form className="userform" action="/api/user/insert" method="post">
-          <input type="text" name="name"placeholder="Write the name" />
-          <input type="text" name="location" placeholder="Write the Location"/>
-          <input type="number" name="age" placeholder="Age"/>
-          <input type="text" name="gender" placeholder="Gender"/>
-          <input type="text" name="job" placeholder="Job"/>
-          <button type="submit" name="button" className="btn">Submit</button>
+      <form className="userform" onSubmit={this.submitFakeUserInsert}>
+          <label style={style}>{this.state.error}</label>
+          <input type="text" name="name"  placeholder="Write the name" ref={this.name} />
+          <input type="text" name="location" placeholder="Write the Location"  ref = {this.location}  />
+          <input type="number" name="age" placeholder="Age" ref = {this.age} />
+          <input type="text" name="gender" placeholder="Gender" ref = {this.gender} />
+          <input type="text" name="job" placeholder="Job" ref = {this.job} />
+          <button type="submit" onClick={this.submitFakeUserInsert} name="button" className="btn">Submit</button>
       </form>
     )
   }
 }
+const mapStateToProps = state => {
+  return {
+    users : state.user,
+    signInUser : state.signInUser
+  }
+}
+const mapDispatchToProps = dispatch => {
+      return {
+
+
+    }
+
+}
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UserForm)
