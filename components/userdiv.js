28e246a15a6d5feac853bcc78c2f106ddb14acbf
anikethsaha <anik220798@gmail.com>
2018-04-23 22:00:48 +0530

    Update userdiv.js

--- components/userdiv.js
+++ components/userdiv.js
@@ -6,13 +6,19 @@ export default class UserDiv extends React.Component{
   }
   render(){
     return (
-      <div className="three columns">
+      <div className="user-div four columns">
         <div className="user_container">
-          <h2><strong>Name : </strong>{ this.props.name}</h2>
-          <h3><strong>Age : </strong>{ this.props.age}</h3>
-          <h3><strong>Gender : </strong>{ this.props.gender}</h3>
-          <p><strong> Location :</strong>{ this.props.location}</p>
-          <p><strong>Jobs :</strong>{ this.props.job}</p>
+          <div className="user_avatar">
+                <img src="./public/image/66468562db3b343617b4912256cad666.png" />
+          </div>
+          <div className="user_details">
+                <h2 className="user_name"><strong>Name : </strong>{ this.props.name}</h2>
+                <h3><strong>Age : </strong>{ this.props.age}</h3>
+                <h3><strong>Gender : </strong>{ this.props.gender}</h3>
+                <p><strong> Location :</strong>{ this.props.location}</p>
+                <p><strong>Jobs :</strong>{ this.props.job}</p>
+          </div>
+
         </div>
       </div>
     )
